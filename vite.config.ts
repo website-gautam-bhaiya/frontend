3ece2d70ff06ed65c4f084f5af311dc18e095169
website-gautam-bhaiya <workthinkfinance@gmail.com>
2025-03-08 16:04:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensures output is placed in "dist"
+  },
   server: {
-    https:{
+    https: {
       key: './../ssl--certificates/decrypted-key.pem',
       cert: './../ssl--certificates/cert.pem'
-    }
+    },
+    historyApiFallback: true, // 👈 Ensures React Router handles routing correctly
   }
 })
